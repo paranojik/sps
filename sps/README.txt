714262fa8d68452094ddbb8e3baa8b5b0bc655ab
Neil Hastings <nhastings@phase2technology.com>
2012-06-15 10:15:53 -0400

    Create initial manager unit tests

--- sps/README.txt
+++ sps/README.txt
@@ -1,5 +1,7 @@
 Please documentation here for the framework
 
+Running Tests:  run the following command from Drupal root:  php scripts/run-tests.sh --color --verbose SPS
+
 SPS (Site Preview System)
 
 SPS Manager
--- sps/lib/SPS/Manager.php
+++ sps/lib/SPS/Manager.php
@@ -2,6 +2,20 @@
 
 namespace SPS;
 
+use SPS\SiteState;
+
 class Manager {
 
+  /**
+   * Get the active Site State
+   *
+   * @return SiteState | NULL
+   *
+  public function getSiteState() {
+    return new SiteState();
+  }
+
+  /**
+   * Get a
+   */
 }
