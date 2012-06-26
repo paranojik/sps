cd7081c464c492dffbe03330208f1ec2afe1d558
e2thex <erik@e2thex.com>
2012-06-26 13:33:42 -0400

    consolidating all storagecontrllers in to one interface

--- sps/lib/Drupal/sps/StorageControllerInterface.php
+++ sps/lib/Drupal/sps/StorageControllerInterface.php
@@ -1,8 +1,31 @@
 <?php
 namespace Drupal\sps;
 interface StorageControllerInterface {
- public function save($table);
- public function getRevisionID($type, $id);
- public function getMap();
- public function hasValidCache();
+ /**
+  * Cache away a object
+  *
+  * @param $name
+  *   A string name use for retrieval
+  * @param $cache
+  *   an object to be cached
+  * @return NULL
+  */
+ public function set($name, $cache);
+ /**
+  * Test if we have an object cached
+  * This should be less expensive then using get
+  *
+  * @param $name
+  *   A string name use for retrieval
+  * @return bool
+  */
+ public function is_set($name);
+ /**
+  * Retrieve a cached object
+  *
+  * @param $name
+  *   A string name use for retrieval
+  * @return the object that was cached
+  */
+ public function get($name);
 }
