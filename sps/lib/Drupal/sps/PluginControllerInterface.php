2d8cc39adcb8b7ca8f8f2ffc612f2dc9f4d66584
Neil Hastings <nhastings@phase2technology.com>
2012-07-06 09:17:02 -0400

    Update the PluginControllerInterface code comments

--- sps/lib/Drupal/sps/PluginControllerInterface.php
+++ sps/lib/Drupal/sps/PluginControllerInterface.php
@@ -3,40 +3,46 @@ namespace Drupal\sps;
 
 interface PluginControllerInterface {
   /**
-   * factory for building a plugin object
+   * Factory for building a plugin object.
    *
-   * @param $type 
+   * The loading of the plugin is not cached.
+   *
+   * @param $type
    *   the type of plugin as defined in hook_sps_plugin_types_info
-   * @param $name 
+   * @param $name
    *   the name of the plugin as defined in hook_sps_PLUGIN_TYPE_plugin_info;
-   * @return 
-   *   an array of meta data for the plugin
+   * @param $manager \Drupal\sps\Manager
+   *
+   * @return
+   *   The plugin object
    */
-  public function getPlugin($type, $name, $manager);
+  public function getPlugin($type, $name, \Drupal\sps\Manager $manager);
 
   /**
    * get meta info on a plugin
    *
-   * @param $type 
+   * @param $type
    *   the type of plugin as defined in hook_sps_plugin_types_info
-   * @param $name 
+   * @param $name
    *   the name of the plugin as defined in hook_sps_PLUGIN_TYPE_plugin_info;
-   * @return 
+   *
+   * @return array
    *   an array of meta data for the plugin or an array of plugin arrays
    */
   public function getPluginInfo($type, $name=NULL);
 
- /**
-   * get meta info on a plugin
+  /**
+   * get meta info on plugins for a given criteria
    *
-   * @param $type 
+   * @param $type
    *   the type of plugin as defined in hook_sps_plugin_types_info
-   * @param $property 
+   * @param $property
    *   the meta property to compare to the value
-   * @param $value 
+   * @param $value
    *   the value to compare to the meta property
-   * @return 
-   *   an array of meta data for the plugins
+   *
+   * @return
+   *   an array of meta data for the plugins that match the criteria
    */
   public function getPluginByMeta($type, $property, $value);
 
