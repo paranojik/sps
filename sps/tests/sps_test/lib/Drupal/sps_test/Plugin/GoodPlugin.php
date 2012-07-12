0cfc414604bef0b80cd11817a9940ee49669d16d
e2thex <erik@e2thex.com>
2012-07-12 17:41:51 -0400

    Change the PluginFactory to use a HookController instead of module_invoke_all module_implements module_invoke and drupal_alter (issue #/1679560)

--- sps/tests/sps_test/lib/Drupal/sps_test/Plugin/GoodPlugin.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\sps_test\Plugin;
-
-use Drupal\sps\Plugins\AbstractPlugin;
-
-class GoodPlugin extends AbstractPlugin implements TestTypeInterface {
-  public function testMethod() {}
-}
