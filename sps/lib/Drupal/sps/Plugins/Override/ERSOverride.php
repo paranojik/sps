ceeb7dd89a3563230374fed19706b7e585a46583
Josh Caldwell <jcaldwell@phase2technology.com>
2012-07-26 10:21:30 -0400

    [#1693124] basic ers override - mostly working

--- sps/lib/Drupal/sps/Plugins/Override/ERSOverride.php
+++ sps/lib/Drupal/sps/Plugins/Override/ERSOverride.php
@@ -1,5 +1,5 @@
 <?php
-namespace Drupal\sps\Override;
+namespace Drupal\sps\Plugins\Override;
 
 use \Drupal\sps\Plugins\Override\NodeDateOverride;
 
@@ -12,11 +12,29 @@ class ERSOverride extends NodeDateOverride {
    */
   public function getOverrides() {
     $select = db_select('ers_schedule', 'es')
-      ->fields('es')
+      ->fields('es', array('schedule_id', 'entity_type', 'entity_id', 'revision_id'))
       ->condition('publish_date', $this->timestamp, '<=')
-      ->condition('completed', 0);
+      ->condition('completed', 0)
+      ->orderBy('publish_date')
+      ->orderBy('revision_id');
 
-    $results = $select->fetchAllAssoc('entity_type');
-    return $results;
+    $results = $select->execute()->fetchAllAssoc('entity_type', \PDO::FETCH_ASSOC);
+
+    $list = array();
+    foreach($results as $key => $result) {
+      if (isset($result['entity_id'])) {
+        $results[$key] = $result = array($result);
+      }
+
+      foreach($result as $sub => $row) {
+        $transform = array();
+        $transform['id'] = $row['entity_id'];
+        $transform['type'] = $row['entity_type'];
+        $transform['revision_id'] = $row['revision_id'];
+        $list[$row['entity_type'].'-'.$row['entity_id']] = $transform;
+      }
+    }
+
+    return $list;
   }
 }
