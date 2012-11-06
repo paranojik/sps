97fd707be0e16de1905994f74e6ff2d627f4a7e5
Neil Hastings <nhastings@phase2technology.com>
2012-11-06 11:58:54 -0500

    Add check to EntityLoadReaction for empty ids which can occur on node reference in some cases.

--- lib/Drupal/sps/Plugins/Reaction/EntityLoadReaction.php
+++ lib/Drupal/sps/Plugins/Reaction/EntityLoadReaction.php
@@ -27,26 +27,30 @@ class EntityLoadReaction implements \Drupal\sps\Plugins\ReactionInterface {
    *   revision_id_key
    *   base_id_key
    *   base_table
-   *   ids : the 
+   *   ids : the
    * @param $override_controller
    *   This is an override controller to use to find
    *   override data
    *
-   * @return 
+   * @return
    *  array of revision ids;
    */
   public function react($data, \Drupal\sps\Plugins\OverrideControllerInterface $override_controller) {
-    $revision_id_query = db_select($data->base_table, 'b');
-    $revision_id_query->fields('b', array($data->revision_id_key, $data->base_id_key));
-    $revision_id_query->condition('b.'. $data->base_id_key, $data->ids);
-    $revision_id_query->addTag(SPS_NO_ALTER_QUERY_TAG);
-    $result = $revision_id_query->execute()->fetchAllAssoc($data->base_id_key); 
     $had_overrides = FALSE;
-    foreach($result as $id => $row) {
-      $override = $override_controller->getRevisionId($id, $data->type);
-      $vids[] = $override['revision_id'] ?: $row->{$data->revision_id_key};
-      $had_overrides = $had_overrides || $override;
+    if (!empty($data->ids)) {
+      $revision_id_query = db_select($data->base_table, 'b');
+      $revision_id_query->fields('b', array($data->revision_id_key, $data->base_id_key));
+      $revision_id_query->condition('b.'. $data->base_id_key, $data->ids);
+      $revision_id_query->addTag(SPS_NO_ALTER_QUERY_TAG);
+      $result = $revision_id_query->execute()->fetchAllAssoc($data->base_id_key);
+
+      foreach($result as $id => $row) {
+        $override = $override_controller->getRevisionId($id, $data->type);
+        $vids[] = $override['revision_id'] ?: $row->{$data->revision_id_key};
+        $had_overrides = $had_overrides || $override;
+      }
     }
+
     return $had_overrides ? $vids : array();
   }
 }
